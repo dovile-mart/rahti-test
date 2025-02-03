5f53b1ba0c3402d79521aab060f9522040d1dfcf
Martinonyte Dovile <h03181@haaga-helia.fi>
2025-02-03 16:32:21 +0200

    Update: testing crud

--- src/main/java/fi/haagahelia/course/domain/StudentRepository.java
+++ src/main/java/fi/haagahelia/course/domain/StudentRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface StudentRepository extends CrudRepository<Student, Long> {
 
+    List<Student> findByLastName(String lastName);
     
 }
